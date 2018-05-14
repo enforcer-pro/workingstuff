be2ab58c8ff665f0cfaaee5b9c539b21324b3989
adfoke <36018810+adfoke@users.noreply.github.com>
2018-05-14 12:38:17 +0800

    Create HibernateTx.java

---
+++ hibernate/HibernateTx.java
@@ -0,0 +1,23 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class HibernateTx {
+    public void Hibernatetx(){
+        SessionFactory sessionFactory=null;
+        Session session=null;
+        Transaction trans=null;
+        try{
+            sessionFactory=HibernateUtil.getSessionFactory();
+            session=HibernateUtil.getSessionObject();
+            trans=session.beginTransaction();
+
+        }catch (Exception e){
+            e.fillInStackTrace();
+            trans.rollback();
+        }finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+}
