617001b6350cc5848f8c4b77ca7a3c7eea432d71
adfoke <36018810+adfoke@users.noreply.github.com>
2018-05-14 12:37:32 +0800

    Update HibernateUtil.java

--- hibernate/HibernateUtil.java
+++ hibernate/HibernateUtil.java
@@ -1,16 +1,22 @@
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
     static Configuration cfg =null;
     static SessionFactory sessionFactory =null;
+    static  Session session =null;
     static {
         cfg= new Configuration();
         cfg.configure();
         sessionFactory=cfg.buildSessionFactory();
+        session = sessionFactory.openSession();
 
     }
     public static SessionFactory getSessionFactory(){
         return sessionFactory;
+    }
+    public static Session getSessionObject(){
+        return session;
     }
 }
